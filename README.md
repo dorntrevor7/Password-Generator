3bbb8a4d465381ac8873981069707658795e95cf
Trevor Dorn <dorntrevor7@gmail.com>
2020-02-06 23:48:18 -0700

    working but not complete

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Password Generator
+
+In this project I created a password generator from a javascript file connected to an HTML and manipulated the DOM of the HTML.
+
+Here is the link: [Password Generator](https://dorntrevor7.github.io/Password-Generator/)
--- script.js
+++ script.js
@@ -1,54 +1,59 @@
 // Assignment Code
+//Call the button on the HTML page
 var generateBtn = document.querySelector("#generate");
 
-var keys = {
-    length: prompt("How long do you want your password to be?");
-    lowerCase: confirm("Would you like to have lower cases in this password?"),
-    upperCase: confirm("Would you like to have upper cases in this password?"),
-    numeric: confirm("Would you like to have numbers in this password?"),
-    special: confirm("Would you like to have special characters in this password?")
-};
-
-var passwordText = document.querySelector("#password");
-passwordText.value = password;
-
-for (var i = 1; i <= keys.length; i++) {
-    var password = [];
-    if (keys.lowerCase === true) {
-        password += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-        console.log(password);
-    }
-    if (keys.upperCase === true) {
-        password += String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-        console.log(password);
-    }
-    if (keys.numeric === true) {
-        password += String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-        console.log(password);
-    }
-    if (keys.special === true) {
-        specialchar = "!@#$%&*?";
-        password += specialchar[Math.floor(Math.random() * specialchar.length)];
-        console.log(password);
-    }
-    return password.slice(0, keys.length);
+//Function runs when the button is clicked 
+function writePassword() {
+    var params = {
+        Length: prompt("How long would you like your password to be?"),
+        lowercase: confirm("Would you like to use lowercase characters?"),
+        uppercase: confirm("Would you like to use uppercase characters?"),
+        specialChars: confirm("Would you like to use special characters?"),
+        numericChars: confirm("Would you like to use numeric characters?")
+    };
+    // generatePassword invokes the info from writePassword function
+    var password = generatePassword(params);
+    // Calling the html for the id password
+    var passwordText = document.querySelector("#password");
+    // Changes the actual html page
+    passwordText.textContent = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", password);
+// runs the length of the length prompt 
+// checks to see if the confirms are true
+function generatePassword(e) {
+    for (var i = 0; i < e.Length; i++) {
+        var pass = "";
+        // password is set to equal and add on a random lowercase character 
+        if (!e.lowerCase) {
+            pass += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+            console.log(pass);
+        }
+        // password is set to equal and add on a random uppercase character 
+        if (!e.upperCase) {
+            pass += String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+            console.log(pass);
+        }
+        // password is set to equal and add on a random numeric character 
+        if (!e.numeric) {
+            pass += String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+            console.log(pass);
+        }
+        // A string that has special chars. which is randomized
+        // through an array with the length of the string
+        if (!e.special) {
+            specialchar = "!@#$%&*?";
+            pass += specialchar[Math.floor(Math.random() * specialchar.length)];
+            console.log(pass);
+        }
+        // password adds the string from pass
+        var password = password + pass;
+        console.log(password);
 
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
+    }
+    // slices the undefined off password and returns
+    return password = password.slice(9);
+
+}
+//Listener to the UI's click to generate generatebtn
+generateBtn.addEventListener("click", writePassword);
